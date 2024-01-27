1454df971247afbe729a81658bd3eacb04e0c420
Kevin Schroeder <kevinschr@ibm.com>
2024-01-27 14:49:33 -0700

    fix typing

--- src/chat.rs
+++ src/chat.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, input::{keyboard::KeyboardInput, ButtonState}};
 
 pub struct ChatPlugin;
 
@@ -8,18 +8,20 @@ pub struct ChatPlugin;
 struct Chat;
 
 #[derive(Resource)]
-struct BackTimer {
-    timer: Timer
+struct Timers {
+    tenth_second: Timer,
+    half_second: Timer,
 }
 
 impl Plugin for ChatPlugin {
     fn build(&self, app: &mut App) {
         app
-            .insert_resource(BackTimer {
-                timer: Timer::new(Duration::from_millis(50), TimerMode::Repeating)
+            .insert_resource(Timers {
+                tenth_second: Timer::new(Duration::from_millis(100), TimerMode::Repeating),
+                half_second: Timer::new(Duration::from_millis(500), TimerMode::Repeating),
             })
             .add_systems(Startup, spawn_chat)
-            .add_systems(Update, chat);
+            .add_systems(Update, (blink_cursor, keyboard_system));
     }
 }
 
@@ -27,7 +29,11 @@ fn spawn_chat(
     mut commands: Commands
 ) {
     commands.spawn((
-        TextBundle::from("test").with_style(
+        TextBundle::from_sections([
+            TextSection::new("", TextStyle::default()),
+            TextSection::new("", TextStyle::default()),
+            TextSection::new("", TextStyle::default())
+        ]).with_style(
             Style {
                 position_type: PositionType::Absolute,
                 bottom: Val::Px(10.0),
@@ -39,57 +45,78 @@ fn spawn_chat(
     ));
 }
 
-fn chat(
+fn blink_cursor(
     mut query: Query<&mut Text, With<Chat>>,
-    keycode: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut backspace_timer: ResMut<BackTimer>,
+    mut timers: ResMut<Timers>,
 ) {
-    backspace_timer.timer.tick(time.delta());
+    timers.tenth_second.tick(time.delta());
+    timers.half_second.tick(time.delta());
 
     let mut text = query.get_single_mut().unwrap();
-    for key in keycode.get_just_pressed() {
-        match key {
-            KeyCode::Back => {
-                text.sections[0].value.pop();
-            },
-            KeyCode::Space => {
-                text.sections[0].value.push(' ');
-            },
-            KeyCode::Tab => {
-                text.sections[0].value.push_str("    ");
-            },
-            KeyCode::Slash => {
-                text.sections[0].value.push('/');
-            },
-            KeyCode::ShiftLeft => {},
-            KeyCode::Underline => {
-                text.sections[0].value.push('_');
-            },
-            KeyCode::Period => {
-                text.sections[0].value.push('.');
-            },
-            _ => {
-                let mut key = format!("{:?}", key);
-                if !keycode.pressed(KeyCode::ShiftLeft) {
-                    key = key.to_lowercase();
-                };
-                text.sections[0].value.push_str(&key);
-            }
+    if timers.half_second.finished() {
+        let cursor_section = &mut text.sections[1].value;
+        if cursor_section.is_empty() {
+            cursor_section.push('|');
+        } else {
+            cursor_section.pop();
         }
     }
+}
 
-    let blink_duration = time.elapsed_seconds_f64() % 1.0;
-    if blink_duration < 0.5 && text.sections.len() == 1 {
-        text.sections.push(TextSection {
-            value: "|".into(),
-            ..default()
-        });
-    } else if text.sections.len() == 2 {
-        text.sections.pop();
-    }
+fn keyboard_system(
+    mut query: Query<&mut Text, With<Chat>>,
+    keycode: Res<Input<KeyCode>>,
+    mut rd: EventReader<KeyboardInput>
+) {
+    let mut text = query.get_single_mut().unwrap();
 
-    if keycode.pressed(KeyCode::Back) && backspace_timer.timer.finished() {
-        text.sections[0].value.pop();
+    for ev in rd.read() {
+        if ev.state != ButtonState::Pressed {
+            continue;
+        }
+        if let Some(key) = ev.key_code {
+            match key {
+                KeyCode::Back => {
+                    text.sections[0].value.pop();
+                },
+                KeyCode::Left => {
+                    let char = text.sections[0].value.pop().unwrap_or_default();
+                    text.sections[2].value.insert(0, char);
+                },
+                KeyCode::Right => {
+                    if !text.sections[2].value.is_empty() {
+                        let char = text.sections[2].value.remove(0);
+                        text.sections[0].value.push(char);
+                    }
+                },
+                KeyCode::Space => {
+                    text.sections[0].value.push(' ');
+                },
+                KeyCode::Tab => {
+                    text.sections[0].value.push_str("    ");
+                },
+                KeyCode::Slash => {
+                    text.sections[0].value.push('/');
+                },
+                KeyCode::Underline => {
+                    text.sections[0].value.push('_');
+                },
+                KeyCode::Period => {
+                    text.sections[0].value.push('.');
+                },
+                _ => {
+                    let mut key = format!("{:?}", key);
+                    if key.len() > 1 {
+                        continue;
+                    }
+    
+                    if !keycode.pressed(KeyCode::ShiftLeft) && !keycode.pressed(KeyCode::ShiftRight) {
+                        key = key.to_lowercase();
+                    };
+                    text.sections[0].value.push_str(&key);
+                }
+            }
+        }
     }
 }
